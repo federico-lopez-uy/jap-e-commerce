a65014d79a33d7510b7e28ec8e3f2f2bf2ca41d3
FedeLopez17 <fedelopez1712@gmail.com>
2025-09-20 15:58:07 -0300

    Fix - Corregir lógica de filtrado y ordenado

--- js/products.js
+++ js/products.js
@@ -1,16 +1,19 @@
 let productos;
+let busqueda, sortOption;
+let precioMinimo = -Infinity;
+let precioMaximo = Infinity;
 
-const inicializarTabla = async () => {
+const inicializarProductos = async () => {
   const catID = localStorage.getItem("catID");
   const res = await getJSONData(
     `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
   );
   productos = res.data.products;
 
-  rellenarTabla(productos);
+  actualizarTabla();
 };
 
-const rellenarTabla = async (productos) => {
+const actualizarTabla = async () => {
   const contenedorProductos = document.querySelector("div.contenedor");
   contenedorProductos.innerHTML = "";
 
@@ -18,7 +21,30 @@ const rellenarTabla = async (productos) => {
   listaProductos.classList.add("product-list");
   contenedorProductos.appendChild(listaProductos);
 
-  for (const producto of productos) {
+  let productosFiltradosOrdenados = productos.filter(
+    (p) => p.cost >= precioMinimo && p.cost <= precioMaximo
+  );
+
+  if (sortOption) {
+    productosFiltradosOrdenados.sort((A, B) =>
+      sortOption == "price-asc"
+        ? A.cost - B.cost
+        : sortOption == "price-des"
+        ? B.cost - A.cost
+        : B.soldCount - A.soldCount
+    );
+  }
+
+  if(busqueda){
+    productosFiltradosOrdenados = productosFiltradosOrdenados.filter((prod) => {
+    return (
+      prod.description.toUpperCase().includes(busqueda) ||
+      prod.name.toUpperCase().includes(busqueda)
+    );
+  });
+  }
+
+  for (const producto of productosFiltradosOrdenados) {
     const tarjetaProducto = document.createElement("section");
     tarjetaProducto.classList.add("product-card");
 
@@ -42,39 +68,37 @@ const rellenarTabla = async (productos) => {
   }
 };
 
-inicializarTabla();
+inicializarProductos();
 
 const botonFiltro = document.getElementById("rangeFilterCount");
-botonFiltro.addEventListener("click", async()=>{
-    const minimo = Number (document.getElementById("rangeFilterCountMin").value) || -Infinity;
-    const maximo = Number (document.getElementById("rangeFilterCountMax").value) || Infinity;
-
-    const productosFiltrados = productos.filter(p => p.cost >= minimo && p.cost <= maximo);
-    rellenarTabla(productosFiltrados)
-})
-
+botonFiltro.addEventListener("click", () => {
+  precioMinimo =
+    Number(document.getElementById("rangeFilterCountMin").value) || -Infinity;
+  precioMaximo =
+    Number(document.getElementById("rangeFilterCountMax").value) || Infinity;
+  actualizarTabla();
+});
 
 const botonLimpiar = document.getElementById("clearRangeFilter");
-botonLimpiar.addEventListener("click", ()=> {
-    rellenarTabla(productos);
-    document.getElementById("rangeFilterCountMin").value = ""
-    document.getElementById("rangeFilterCountMax").value = ""
-})
+botonLimpiar.addEventListener("click", () => {
+  [...document.getElementsByClassName("range-filter")].forEach(ele => ele.value = "")
+  precioMinimo = -Infinity;
+  precioMaximo = Infinity;
+  actualizarTabla();
+});
 
 const botonesFiltro = document.getElementsByName("sortOptions");
-botonesFiltro.forEach(boton => boton.addEventListener("change", (e)=> {
-  const optionSelected = e.target.value;
+botonesFiltro.forEach((boton) =>
+  boton.addEventListener("change", (e) => {
+    sortOption = e.target.value;
+    actualizarTabla();
+  })
+);
 
-  let productosOrdenados;
-
-  if(optionSelected == "price-asc") {
-    productosOrdenados = productos.slice().sort((A, B) => A.cost - B.cost);
-  } else if (optionSelected == "price-des") {
-    productosOrdenados = productos.slice().sort((A, B) => B.cost - A.cost);
-  } else {
-    productosOrdenados = productos.slice().sort((A, B) => B.soldCount - A.soldCount);
-  }
-
-  rellenarTabla(productosOrdenados);
-
-}))
+const barraBusqueda = document.getElementById("searchInput");
+let timeoutBusqueda;
+barraBusqueda.addEventListener("input", () => {
+  busqueda = barraBusqueda.value.toUpperCase().trim();
+  if (timeoutBusqueda) clearTimeout(timeoutBusqueda);
+  timeoutBusqueda = setTimeout(actualizarTabla, 250);
+});
